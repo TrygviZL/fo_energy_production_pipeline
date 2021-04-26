2b206bf188b52e5e69f5a0042aba37a493a1bfb1
TrygviZL <56014816+TrygviZL@users.noreply.github.com>
2021-04-26 16:26:11 +0200

    added destroy policy and sort key

--- lib/fo_energy_production_pipeline-stack.ts
+++ lib/fo_energy_production_pipeline-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import { RemovalPolicy } from '@aws-cdk/core';
 
 export class FoEnergyProductionPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -21,13 +22,15 @@ export class FoEnergyProductionPipelineStack extends cdk.Stack {
     // DynamoDB for storing the SEV data
     const SevTable = new dynamodb.Table(this, 'Table', {
       partitionKey: {
-        name: 'PK',
-        type: dynamodb.AttributeType.STRING,
+        name: 'Date',
+        type: dynamodb.AttributeType.STRING
+      },
+      sortKey:{
+        name: 'Time',
+        type: dynamodb.AttributeType.STRING
       },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY
     });
-
-    new cdk.CfnOutput(this, 'TableName', {value: SevTable.tableName});
-
   }
 }
