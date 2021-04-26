24884e14e5749743e7966a4c409c091858394059
TrygviZL <56014816+TrygviZL@users.noreply.github.com>
2021-04-26 19:21:41 +0200

    Added pushtolambda and cron job

--- lambda/pull_data.js
+++ lambda/pull_data.js
@@ -1,45 +1,71 @@
 var https = require('https');
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB();
+AWS.config.update({region: 'eu-west-1'});
+
+
 
 // Primary handler function for lambda to pull data
 exports.handler = async function(event) {
-    return httprequest().then((data) => {
+    console.log("request:", JSON.stringify(event, undefined, 2));
+
+    httprequest().then((data)=>{
+    
         const response = {
             statusCode: 200,
             body: JSON.stringify(data),
         };
-    return response;
-    });
+        var parsed = JSON.parse(response.body)
+    
+        const params = {
+            TableName: 'energyDataFO',
+            Item: {
+                'Date': {S: parsed.tiden.split(" ")[0]},
+                'Time': {S: parsed.tiden.split(" ")[1]},
+                'Oil': {N: parsed.OlieSev_E.replace(/,/g, '.')},
+                'Water': {N: parsed.VandSev_E.replace(/,/g, '.')},
+                'Wind': {N: parsed.VindSev_E.replace(/,/g, '.')},
+                'Biogas': {N :parsed.BiogasSev_E.replace(/,/g, '.')},
+                'Sun': {N: parsed.SolSev_E.replace(/,/g, '.')}
+            }
+        }
+
+        dynamodb.putItem(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data);
+        }
+        })
+    
+    })
 };
 
 // Function to call API and return data
 function httprequest() {
-     return new Promise((resolve, reject) => {
-        const options = {
-            hostname: 'www.sev.fo',
-            path: '/api/realtimemap/now',
-            method: 'GET'
-        };
-        const req = https.request(options, (res) => {
-          if (res.statusCode < 200 || res.statusCode >= 300) {
-                return reject(new Error('statusCode=' + res.statusCode));
-            }
-            var body = [];
-            res.on('data', function(chunk) {
-                body.push(chunk);
-            });
-            res.on('end', function() {
-                try {
-                    body = JSON.parse(Buffer.concat(body).toString());
-                } catch(e) {
-                    reject(e);
-                }
-                resolve(body);
-            });
-        });
-        req.on('error', (e) => {
-          reject(e.message);
-        });
-        // send the request
-       req.end();
-    });
+    return new Promise((resolve, reject) => {
+       const options = {
+           hostname: 'www.sev.fo',
+           path: '/api/realtimemap/now',
+           method: 'GET'
+       };
+       const req = https.request(options, (res) => {
+           var body = [];
+           res.on('data', function(chunk) {
+               body.push(chunk);
+           });
+           res.on('end', function() {
+               try {
+                   body = JSON.parse(Buffer.concat(body).toString());
+               } catch(e) {
+                   reject(e);
+               }
+               resolve(body);
+           });
+       });
+       // send the request
+      req.end();
+   });
 }
+
+
--- lib/fo_energy_production_pipeline-stack.ts
+++ lib/fo_energy_production_pipeline-stack.ts
@@ -1,23 +1,13 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
-import * as apigw from '@aws-cdk/aws-apigateway';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import { RemovalPolicy } from '@aws-cdk/core';
+import * as targets from '@aws-cdk/aws-events-targets'
+import * as events from '@aws-cdk/aws-events'
 
 export class FoEnergyProductionPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const SevPullLambda = new lambda.Function(this, 'PullData', {
-      runtime: lambda.Runtime.NODEJS_12_X,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'pull_data.handler'
-    });
-
-    // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: SevPullLambda
-    });
 
     // DynamoDB for storing the SEV data
     const SevTable = new dynamodb.Table(this, 'Table', {
@@ -32,5 +22,18 @@ export class FoEnergyProductionPipelineStack extends cdk.Stack {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       removalPolicy: RemovalPolicy.DESTROY
     });
+
+    const SevPullLambda = new lambda.Function(this, 'PullData', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'pull_data.handler',
+      environment: {
+        TABLE_NAME: SevTable.tableName
+      }
+    });
+    const eventRule = new events.Rule(this, 'scheduleRule', {
+      schedule: events.Schedule.cron({ minute: '/1'}),
+    });
+    eventRule.addTarget(new targets.LambdaFunction(SevPullLambda))
   }
 }
